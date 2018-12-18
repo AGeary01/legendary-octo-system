84dfaf1540d5bbe2bef16ed35c9b4d088099978f
SUSSEXDOWNS\472555 <472555@chrome.sussexdowns.ac.uk>
2018-12-18 13:12:25 +0000

    no message

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+    }
+}
