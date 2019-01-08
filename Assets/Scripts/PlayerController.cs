f2ba6a72159767a149021d68403f590a4b88532a
SUSSEXDOWNS\472555 <472555@chrome.sussexdowns.ac.uk>
2019-01-08 15:08:12 +0000

    no message

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,21 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     private void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+
+        rb.AddForce (movement * speed);
     }
 }
